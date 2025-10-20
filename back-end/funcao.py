899742caf5e4198bc029eafd9d37d3dfdf8779b2
Joanna Santos <joanna.d.souza@aluno.senai.br>
2025-10-20 11:28:08 -0300

    função atualizar e deletar produto

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -54,4 +54,32 @@ def listar_products():
         finally:
             cursor.close()
             conexao.close()
-            
+
+def atualizar_products(id_produtos, novo_preco):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDATE movies SET avaliacao = %s WHERE id = %s",
+                (novo_preco, id_produtos)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao atualizar o preco do produto {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+def deletar_produto(id_produto):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                    "DELETE FROM movies WHERE id = %s", (id_produto,)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao deletar filme: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
